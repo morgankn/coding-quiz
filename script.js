10b9e2385dd713ca3b186955fab7d847fd729caf
morgankn <118085617+morgankn@users.noreply.github.com>
2022-12-17 13:47:54 -0500

    added CSS to quiz

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById('start')
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+console.log('Started')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
